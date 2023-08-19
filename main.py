931f85d6a2c103f211efe30c53337de9a0862521
SpencerSedano <spencer.sv20@gmail.com>
2023-08-20 00:35:11 +0800

    quotes

--- main.py
+++ main.py
@@ -13,11 +13,16 @@ app = FastAPI()
 
 quotes = []
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello there"}
 
 @app.post("/quotes")
-async def get_quotes(quote: Quote):
+async def post_quotes(quote: Quote):
     quotes.append(quote)
     return {"quotes": "Information was created"}
+
+@app.get("/quotes")
+async def get_quotes():
+    return {"quotes": quotes}
