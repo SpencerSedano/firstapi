92175eb27a43b42132f2e472375fba3b875af054
SpencerSedano <spencer.sv20@gmail.com>
2023-08-19 12:02:18 +0800

    commiting

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Quote(BaseModel):
+    id: int
+    name: str
+    quote: str
+    date: str | None = None
+
+
+app = FastAPI()
+
+
+quotes = []
+
+@app.get("/")
+async def root():
+    return {"message": "Hello there"}
+
+@app.post("/quotes")
+async def get_quotes(quote: Quote):
+    quotes.append(quote)
+    return {"quotes": "Information was created"}
